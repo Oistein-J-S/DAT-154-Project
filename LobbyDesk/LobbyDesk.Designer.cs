1548de31d33d2e46791ad69a0b32f90296486a27
Kenneth <thejack388@gmail.com>
2018-02-21 15:53:04 +0100

    Fixed name for form in LobbyDesk

---
+++ LobbyDesk/LobbyDesk.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace LobbyDesk
+{
+    partial class LobbyDesk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label3_3 = new System.Windows.Forms.Label();
+            this.label3_2 = new System.Windows.Forms.Label();
+            this.label3_1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2_3 = new System.Windows.Forms.Label();
+            this.label2_2 = new System.Windows.Forms.Label();
+            this.label2_1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1_2 = new System.Windows.Forms.Label();
+            this.label1_1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.checkingOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkingInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.roomsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Controls.Add(this.label1_2);
+            this.panel1.Controls.Add(this.label1_1);
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(934, 707);
+            this.panel1.TabIndex = 0;
+            this.panel1.Visible = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.tableLayoutPanel2);
+            this.panel2.Controls.Add(this.label2_3);
+            this.panel2.Controls.Add(this.label2_2);
+            this.panel2.Controls.Add(this.label2_1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(934, 683);
+            this.panel2.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.tableLayoutPanel3);
+            this.panel3.Controls.Add(this.label3_3);
+            this.panel3.Controls.Add(this.label3_2);
+            this.panel3.Controls.Add(this.label3_1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(934, 683);
+            this.panel3.TabIndex = 4;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 3;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.88312F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 83.11688F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 86F));
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(18, 51);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.Padding = new System.Windows.Forms.Padding(3);
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(406, 605);
+            this.tableLayoutPanel3.TabIndex = 7;
+            // 
+            // label3_3
+            // 
+            this.label3_3.AutoSize = true;
+            this.label3_3.Location = new System.Drawing.Point(354, 21);
+            this.label3_3.Name = "label3_3";
+            this.label3_3.Size = new System.Drawing.Size(56, 13);
+            this.label3_3.TabIndex = 6;
+            this.label3_3.Text = "Check out";
+            // 
+            // label3_2
+            // 
+            this.label3_2.AutoSize = true;
+            this.label3_2.Location = new System.Drawing.Point(170, 21);
+            this.label3_2.Name = "label3_2";
+            this.label3_2.Size = new System.Drawing.Size(66, 13);
+            this.label3_2.TabIndex = 5;
+            this.label3_2.Text = "Guest Name";
+            // 
+            // label3_1
+            // 
+            this.label3_1.AutoSize = true;
+            this.label3_1.Location = new System.Drawing.Point(15, 20);
+            this.label3_1.Name = "label3_1";
+            this.label3_1.Size = new System.Drawing.Size(55, 13);
+            this.label3_1.TabIndex = 4;
+            this.label3_1.Text = "Room No.";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.88312F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 83.11688F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 86F));
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(18, 66);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.Padding = new System.Windows.Forms.Padding(3);
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(406, 605);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // label2_3
+            // 
+            this.label2_3.AutoSize = true;
+            this.label2_3.Location = new System.Drawing.Point(354, 36);
+            this.label2_3.Name = "label2_3";
+            this.label2_3.Size = new System.Drawing.Size(70, 13);
+            this.label2_3.TabIndex = 2;
+            this.label2_3.Text = "Checking out";
+            // 
+            // label2_2
+            // 
+            this.label2_2.AutoSize = true;
+            this.label2_2.Location = new System.Drawing.Point(170, 36);
+            this.label2_2.Name = "label2_2";
+            this.label2_2.Size = new System.Drawing.Size(66, 13);
+            this.label2_2.TabIndex = 1;
+            this.label2_2.Text = "Guest Name";
+            // 
+            // label2_1
+            // 
+            this.label2_1.AutoSize = true;
+            this.label2_1.Location = new System.Drawing.Point(15, 35);
+            this.label2_1.Name = "label2_1";
+            this.label2_1.Size = new System.Drawing.Size(55, 13);
+            this.label2_1.TabIndex = 0;
+            this.label2_1.Text = "Room No.";
+            this.label2_1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(15, 90);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 535);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // label1_2
+            // 
+            this.label1_2.AutoSize = true;
+            this.label1_2.Location = new System.Drawing.Point(156, 60);
+            this.label1_2.Name = "label1_2";
+            this.label1_2.Size = new System.Drawing.Size(31, 13);
+            this.label1_2.TabIndex = 3;
+            this.label1_2.Text = "Type";
+            // 
+            // label1_1
+            // 
+            this.label1_1.AutoSize = true;
+            this.label1_1.Location = new System.Drawing.Point(36, 60);
+            this.label1_1.Name = "label1_1";
+            this.label1_1.Size = new System.Drawing.Size(55, 13);
+            this.label1_1.TabIndex = 1;
+            this.label1_1.Text = "Room No.";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkingOutToolStripMenuItem,
+            this.checkingInToolStripMenuItem,
+            this.roomsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.menuStrip1.Size = new System.Drawing.Size(934, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // checkingOutToolStripMenuItem
+            // 
+            this.checkingOutToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.checkingOutToolStripMenuItem.Name = "checkingOutToolStripMenuItem";
+            this.checkingOutToolStripMenuItem.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.checkingOutToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
+            this.checkingOutToolStripMenuItem.Text = "Checking out";
+            this.checkingOutToolStripMenuItem.Click += new System.EventHandler(this.checkingOutToolStripMenuItem_Click);
+            // 
+            // checkingInToolStripMenuItem
+            // 
+            this.checkingInToolStripMenuItem.Name = "checkingInToolStripMenuItem";
+            this.checkingInToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.checkingInToolStripMenuItem.Text = "Checking in";
+            this.checkingInToolStripMenuItem.Click += new System.EventHandler(this.checkingInToolStripMenuItem_Click);
+            // 
+            // roomsToolStripMenuItem
+            // 
+            this.roomsToolStripMenuItem.Name = "roomsToolStripMenuItem";
+            this.roomsToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.roomsToolStripMenuItem.Text = "Rooms";
+            this.roomsToolStripMenuItem.Click += new System.EventHandler(this.roomsToolStripMenuItem_Click);
+            // 
+            // LobbyDesk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 707);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "LobbyDesk";
+            this.Text = "LobbyDesk";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2_1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1_2;
+        private System.Windows.Forms.Label label1_1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem roomsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkingInToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkingOutToolStripMenuItem;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label3_3;
+        private System.Windows.Forms.Label label3_2;
+        private System.Windows.Forms.Label label3_1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label2_3;
+        private System.Windows.Forms.Label label2_2;
+    }
+}
+
--- LobbyDesk/Program.cs
+++ LobbyDesk/Program.cs
@@ -16,7 +16,7 @@ namespace LobbyDesk
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LobbyDesk());
         }
     }
 }
