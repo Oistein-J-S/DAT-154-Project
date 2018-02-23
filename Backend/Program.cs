ac61f29c60da61e7bf40291c6a6ee0be65b7b2ca
Oistein-J-S <tha_pzycho@hotmail.com>
2018-02-23 18:49:03 +0100

    Moved code to program

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace Backend
 {
+    /**
+   * Aplication entry
+   * Pure fabrication main function to get the aplication running.
+   * Allows more decupling of project files. 
+   */
     class Program
     {
         static void Main(string[] args)
         {
+            //Some pure fabrication!
+            ModelView mod = new ModelView(); // Model view creates the model that creates the DB manager that connects to the db that exists
+            Console.WriteLine("Starting backend");
+            Console.ReadKey();
+            //Console.ReadLine;
+            //TODO the various Views?
         }
     }
 }
--- Bootstrapper/Strapper.cs
+++ Bootstrapper/Strapper.cs
@@ -17,7 +17,7 @@ namespace Bootstrapper
         static void Main(string[] args)
         {
             //Some pure fabrication!
-            new ModelView(); // Model view creates the model that creates the DB manager that connects to the db that exists
+            ModelView mod = new ModelView(); // Model view creates the model that creates the DB manager that connects to the db that exists
             Console.WriteLine("Starting backend");
             Console.ReadKey();
             //Console.ReadLine;
