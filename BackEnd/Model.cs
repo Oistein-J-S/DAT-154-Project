ff9ea2dffdb1063a30f5338df511be61171c485e
Oistein-J-S <tha_pzycho@hotmail.com>
2018-02-23 15:06:26 +0100

    Added still more methods and commments

--- BackEnd/Model.cs
+++ BackEnd/Model.cs
@@ -28,53 +28,91 @@ namespace Backend
         {   
             return false;
         }
-        public List<Booking> GetBookings(DateTime from = DateTime.now, DateTime to = DateTime.now)
+
+        public List<Booking> GetBookings(DateTime from = DateTime.now, DateTime to = DateTime.now) // Bookings within a period
         {
             return null;
         }
 
-        public List<Booking> GetBookings(int roomNumer)
+        public List<Booking> GetBookings(int roomNumer) // Bookings asociated with a room
         {
             return null;
         }
 
-        public List<Booking> GetBookings(string romType)
+        public List<Booking> GetBookings(string romType) // Bookings asociated with room type
         {
             return null;
         }
 
-        public List<Room> GetAvailibleRooms(DateTime from = DateTime.now, DateTime to = DateTime.now)
+        public List<Booking> GetBookings(string fName, string lName) // Bookings asociated with a customeer
         {
             return null;
         }
 
-        public List<Room> GetAvailibleRooms(string type, DateTime from = DateTime.now, DateTime to = DateTime.now)
+        public List<Room> GetAvailibleRooms(DateTime from = DateTime.now, DateTime to = DateTime.now) //Avalible rooms within date
         {
             return null;
         }
 
-        public List<Room> GetAllRooms(string type, DateTime from = DateTime.now, DateTime to = DateTime.now)
+        public List<Room> GetAvailibleRooms(string type, DateTime from = DateTime.now, DateTime to = DateTime.now) // Availible rooms of type within dates
         {
             return null;
         }
 
-        public List<Room> getOccupiedRooms(DateTime from = DateTime.now, DateTime to = DateTime.now)
+        public List<Room> GetAllRooms() // All roms
         {
             return null;
         }
 
-        public List<Room> getOccupiedRooms(string Type, DateTime from = DateTime.now, DateTime to = DateTime.now)
+        public List<Room> GetAllRooms(string type) //All rooms of type
         {
             return null;
         }
 
+        public List<Room> getOccupiedRooms() //Roms that are ocupied
+        {
+            return null;
+        }
 
+        public List<Room> getOccupiedRooms(string Type) //Roms that are ocupied of type
+        {
+            return null;
+        }
+
+        public bool checkIn()
+        {
+            return false;
+        }
+
+        //Cange signature?
+        public bool checkOut()
+        {
+            return false;
+        }
+
+        public int GetDebt(int roomNumber) //get the dept from room scervice etc from a room
+        {
+            return null;
+        }
+
+        public int GetDebt(string fName, string lName) // get the dept from room scervice etc for a customer
+        {
+            return null;
+        }
+
+        public bool RemoveReservation(int romNumber, DateTime from) // Remove a reservation from a room
+        {
+            return false;
+        }
+
+        public Booking GetBooking(int romNumber, DateTime from) // get booking from a room
+        {
+            return null;
+        }
+
+   
         /*
-         * Ideas for functions:
-         * "makeBooking" takes in info (subs to event from views) from views (web + desktop), sends it to Model for use
-         * "checkIn" and "checkOut" 
-         * "deleteBooking"
-         * "searchBooking" takes info from event, sends info to model, model returns list, send event with list
+         * Ideas for functions: 
          * "viewBooking" get info from app, search model, return object
          * "raiseIssue" subs to even from desktop and app
          * "payment"
